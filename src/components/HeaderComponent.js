210370ff423245315364204c7a57e9e0057996cd
Bobby Keel <bobbykeel76@gmail.com>
2021-09-15 17:01:42 -0500

    changed breakpoint for lg for all, just supports a mobile version less than lg and desktop lg >

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -27,8 +27,8 @@ function Header() {
   return (
     <div>
       <div className="container-fluid shadow p-3 mb-2 bg-body rounded">
-        <div className="row d-flex align-items-center">
-          <div className="col-1 col-md-6 bg-white d-flex justify-content-start justify-content-md-center order-md-3">
+        <div className="row d-flex align-items-center justify-content-evenly">
+          <div className="col-1 col-lg-6 bg-white d-flex justify-content-start justify-content-lg-center order-lg-3">
             <Navbar className="bg-white" light sticky="top" expand="lg" color="light">
               <NavbarToggler style={{ color: "black" }} onClick={() => setToggleNav(!toggleNav)} onBlur={() => setToggleNav(false)} />
               <Collapse isOpen={toggleNav} navbar>
@@ -62,14 +62,14 @@ function Header() {
               </Collapse>
             </Navbar>
           </div>
-          <div className="col-10 col-md-1 order-md-1">
+          <div className="col-10 col-lg-1 order-lg-1">
             <a className="navbar-brand text-dark" href="/home">Wurklo</a>
           </div>
-          <div className="col-1 col-md-2 d-flex justify-content-end align-items-center order-md-4">
+          <div className="col-1 col-lg-2 d-flex justify-content-end align-items-center order-lg-4">
             <Avatar src='https://source.unsplash.com/random/800' />
-            <h4 className="fs-6 fw-bold ps-2 pt-2 d-none d-md-block">Bobby Keel</h4>
+            <h4 className="fs-6 fw-bold ps-2 pt-2 d-none d-lg-block">Bobby Keel</h4>
           </div>
-          <div className="col-md-2 order-md-2">
+          <div className="col-lg-2 order-lg-2">
             <Form className="header__search bg-secondary bg-opacity-50">
               <FormGroup>
                 <Input
@@ -84,7 +84,7 @@ function Header() {
               <Button className="d-none" onClick={handleSubmit} type="submit">Submit</Button>
             </Form>
           </div>
-          <div className="col-md-1 order-md-5 d-none d-md-block">
+          <div className="col-lg-1 order-lg-5 d-none d-lg-block">
             <Link className="nav-link" to="/login">
               <i className="fa fa-sign-in fa-2x text-secondary bg-opacity-50" />
             </Link>
